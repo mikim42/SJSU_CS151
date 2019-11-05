6f794714472a111afe84512f51bb647c2f85d789
kimminseok <mikim@student.42.us.org>
2019-11-05 10:57:45 -0800

    Assignment 3

---
+++ Assignment3/CanvasEditor.java
@@ -0,0 +1,50 @@
+package MVC;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class CanvasEditor implements MouseListener {
+	private Shape currentShape;
+	private JTextField statusText;
+	
+	public CanvasEditor(Shape initialShape) {
+		this.currentShape = initialShape;
+	}
+	
+	public CanvasEditor(Shape initialShape, JTextField textfield) {
+		this.currentShape = initialShape;
+		this.statusText = textfield;
+	}
+	
+	public void setCurrentShape(Shape newShape, JTextField textfield) {
+		this.currentShape = newShape;
+		this.statusText = textfield;
+	}
+	
+	public void setCurrentFill(boolean f) {
+		this.currentShape.setFill(f);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (currentShape == null)
+			return;
+		Shape newShape = (Shape)currentShape.clone();
+
+		newShape.setCenter(e.getX(), e.getY());
+		DrawingCanvas drawingCanvas = (DrawingCanvas)e.getSource();
+		drawingCanvas.addShape(newShape);
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+}
