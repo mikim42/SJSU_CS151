c487c3fe86d201d1bf086cc404c90e721f5b0005
kimminseok <mikim@student.42.us.org>
2020-02-11 12:39:45 -0800

    Revert "moving out individual work"
    This reverts commit cd8dc6d99d174ab281c937c380359320b405db8f.

---
+++ Assignment2/MyTest.java
@@ -0,0 +1,35 @@
+package Assignment2;
+
+public class MyTest {
+	public static void main (String[] args) {
+		Mobile m = new Mobile("Iphone", 1000.00);
+		Contract c1 = new Contract(40, 12, m );
+		Contract c2 = null;
+		Contract c3 = null;
+		try {
+			c2 = c1.clone();
+			c3 = c1.clone();
+		}
+		catch (CloneNotSupportedException ex){
+			System.out.println("There was a problem with copying the contract");
+		}
+		c3.setPlan(20);
+		c3.setMonth(24);
+		c3.getSmartphone().setDescription("Galaxy");
+		c3.getSmartphone().setPrice(500.00);
+		
+		System.out.println("-----------------------\n");
+		System.out.println("Original Contact");
+		System.out.println(c1.toString());
+		System.out.println("------------------------\n");
+		System.out.println("Copied Contract");
+		System.out.println(c2.toString());
+		System.out.println("-----------------------\n");
+		System.out.println("Original Contact");
+		System.out.println(c1.toString());
+		System.out.println("-----------------------\n");
+		System.out.println("Modified Contract");
+		System.out.println(c3.toString());
+		
+	}
+}
